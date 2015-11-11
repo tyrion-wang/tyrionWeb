87c39d1651e1532aca83f5ddef6d1203177e1188
酒枫子 <maple_leaf_7@msn.com>
2015-11-11 13:10:55 +0800

    auto_deploy

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -1,5 +1,5 @@
 class GuestController < ApplicationController
   def visit
-    render :json => {code: 1, msg: 'guest.visit', data: 'Wellcome'} and return
+    render :json => {code: 1, msg: 'guest.visit', data: "Wellcome to Tyrion's web!"} and return
   end
 end
