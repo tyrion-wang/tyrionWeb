183d81aa207b213508f1325e3818f89f093a0936
Tyrion <maple_leaf_7@msn.com>
2015-11-16 00:13:31 +0800

    push

--- public/html/content/js/party_routes.js
+++ public/html/content/js/party_routes.js
@@ -4,12 +4,7 @@
 party.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
     $urlRouterProvider.when("", "/guest");
     $stateProvider
-        .state("user", {  // 个人
-            abstract: true,
-            templateUrl: "/html/user/user.html",
-            controller: "user_Ctrl"
-        })
-
+        
         //游客界面
         .state("guest", {
             url: "/guest",
